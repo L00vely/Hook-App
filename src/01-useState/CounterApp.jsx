d59e3c5066dcc9cfa7a3fba013d6d8429c339932
L00vely <peralesrodriguezdavid@gmail.com>
2023-06-16 12:09:16 -0600

    Breaking bad quotes

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,30 +1,32 @@
-import { useState } from "react";
-
+import { useState } from 'react';
 
 export const CounterApp = () => {
-    const [ state , setCounter ] = useState({
+
+    const [ state, setCounter] = useState({
         counter1: 10,
         counter2: 20,
-        counter3: 30
+        counter3: 30,
     });
 
     const { counter1, counter2, counter3 } = state;
-    
-    return (
-        <>
-            <h1>Counter1: {counter1}</h1>
-            <h2>Counter2: {counter2}</h2>
-            <h2>Counter3: {counter3}</h2>
 
-            <hr />
+  return (
+    <>
+        <h1>Counter1: { counter1 }</h1>
+        <h1>Counter2: { counter2 }</h1>
+        <h1>Counter3: { counter3 }</h1>
 
-            <button className="btn" onClick={ 
+        <hr />
+
+        <button 
+            className="btn" 
+            onClick={ 
                 () => setCounter({
                     ...state,
-                    counter1: counter1 + 1
+                    counter1: counter1 + 1,
                 })
             }>+1</button>
-
-        </>
-    )
+    
+    </>
+  )
 }
